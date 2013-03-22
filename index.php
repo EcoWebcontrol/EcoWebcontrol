51c429dce6b87751f5f200d45ccd348aefede3a9
mrpixeldream <admin@inspiration-host.de>
2013-03-22 12:18:11 +0100

    Changed Froxlor to EcoWebcontrol on some places.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of the Froxlor project.
+ * This file is part of the EcoWebcontrol project.
  * Copyright (c) 2003-2009 the SysCP Team (see authors).
- * Copyright (c) 2010 the Froxlor Team (see authors).
+ * Copyright (c) 2010 the EcoWebcontrol Team (see authors).
  *
  * For the full copyright and license information, please view the COPYING
  * file that was distributed with this source code. You can also view the
@@ -11,7 +11,7 @@
  *
  * @copyright  (c) the authors
  * @author     Florian Lippert <flo@syscp.org> (2003-2009)
- * @author     Froxlor team <team@froxlor.org> (2010-)
+ * @author     EcoWebcontrol team <team@froxlor.org> (2010-)
  * @license    GPLv2 http://files.froxlor.org/misc/COPYING.txt
  * @package    Panel
  *
